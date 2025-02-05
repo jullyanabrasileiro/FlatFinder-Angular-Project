2d249133a7b4892f3aaa425521aa35cc27e08f7f
Jullyana <jullyana.brasileiro@hotmail.com>
2025-02-04 21:42:27 -0800

    mongo db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log('MongoDB connection error:', err));
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
