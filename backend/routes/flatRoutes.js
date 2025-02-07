9ea9829d083ed98680f3995a10134c4b917f208a
Jullyana <jullyana.brasileiro@hotmail.com>
2025-02-06 23:08:07 -0800

    message connection

--- backend/routes/flatRoutes.js
+++ backend/routes/flatRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const { verifyToken, isFlatOwner } = require('../middleware/auth.js');
 const FlatController = require('../controllers/flatController.js');
 
+//new changes - to connect messages
+const messageRoutes = require('./messageRoutes.js');
+router.use('/:id/messages', messageRoutes);
+
+
 router.get('/', verifyToken, FlatController.getAllFlats);
 router.get('/:id', verifyToken, FlatController.getFlatById);
 router.post('/', verifyToken, FlatController.addFlat);
 router.patch('/:id', verifyToken, isFlatOwner, FlatController.updateFlat);
 router.delete('/:id', verifyToken, isFlatOwner, FlatController.deleteFlat);
+
 
 module.exports = router;
 
